1af25ab2fc6bc6a8fe1f1e0db09aaa0ea40d0c96
AYUMIAYUMI <ha.jd.12b@gmail.com>
2023-02-28 23:02:10 +0900

    パスワードの英数字のバリデーション記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,20 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password, format: { with: VALID_PASSWORD_REGEX }
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   validates :nick_name, presence: true
   validates :first_name_kannji, presence: true
   validates :second_name_kannji, presence: true
-  validates :first_name_kana, presence: true
-  validates :second_name_kana, presence: true
+  validates :first_name_kana, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'はカタカナで入力して下さい。'
+  validates :second_name_kana, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'はカタカナで入力して下さい。'
   validates :date_birth, presence: true
+
 
   has_many :items
   has_many :buyers
