de1ecb1540c50cb56339c2876643d3c98351e823
AYUMIAYUMI <ha.jd.12b@gmail.com>
2023-03-04 20:15:54 +0900

    バリデーション変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,8 +11,8 @@ class User < ApplicationRecord
   validates :nick_name, presence: true
   validates :first_name_kannji, presence: true
   validates :second_name_kannji, presence: true
-  validates :first_name_kana, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'はカタカナで入力して下さい。'
-  validates :second_name_kana, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'はカタカナで入力して下さい。'
+  validates :first_name_kana, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'はカタカナで入力して下さい。' }
+  validates :second_name_kana, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'はカタカナで入力して下さい。'}
   validates :date_birth, presence: true
 
 
