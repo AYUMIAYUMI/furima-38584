3275dadbe66528e7f07e616b4f788bd31a2275b4
AYUMIAYUMI <ha.jd.12b@gmail.com>
2023-03-05 19:16:59 +0900

    テストコード記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,13 +8,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nick_name, presence: true
+  validates :nick_name, presence: true, length: { maximum: 40 }
   validates :first_name_kannji, presence: true
   validates :second_name_kannji, presence: true
   validates :first_name_kana, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'はカタカナで入力して下さい。' }
   validates :second_name_kana, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'はカタカナで入力して下さい。'}
   validates :date_birth, presence: true
-
 
   has_many :items
   has_many :buyers
