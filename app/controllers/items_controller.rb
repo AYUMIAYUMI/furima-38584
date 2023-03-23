3621b8bdc2d7f36bf273425a38748bc846f7dfd7
AYUMIAYUMI <ha.jd.12b@gmail.com>
2023-03-23 21:48:59 +0900

    ログインページへ遷移する

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  before_action :move_to_signed_in, except: [:index]
+
   def index
     @items = Item.order('created_at DESC')
   end
@@ -21,5 +23,11 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:product_name, :discription, :image, :category_id, :condition_id, :price, :shipping_info_id,
                                  :shipping_area_id, :shipping_day_id).merge(user_id: current_user.id)
+  end
+
+  def move_to_signed_in
+    unless user_signed_in?
+      redirect_to  '/users/sign_in'
+    end
   end
 end
