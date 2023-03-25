249b5102f90dc514bb079bac981e19b497189e30
AYUMIAYUMI <ha.jd.12b@gmail.com>
2023-03-25 09:00:58 +0900

    出品した商品を一覧表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,8 @@ class ItemsController < ApplicationController
   before_action :move_to_signed_in, except: [:index]
 
   def index
-  #  @items = Item.order('created_at DESC') 一覧表示機能で実装する内容のため現段階ではコメントアウト
+    @items = Item.order('created_at DESC')# created_atは、作成された日時なので 降順 DESC で新しい投稿が上にくるようにできる
+    @items = Item.all
   end
 
   def new
@@ -18,11 +19,13 @@ class ItemsController < ApplicationController
     end
   end
 
+  
+
   private
 
   def item_params
     params.require(:item).permit(:product_name, :discription, :image, :category_id, :condition_id, :price, :shipping_info_id,
-                                 :shipping_area_id, :shipping_day_id).merge(user_id: current_user.id)
+                                 :shipping_area_id, :shipping_day_id, :image).merge(user_id: current_user.id)
   end
 
   def move_to_signed_in
